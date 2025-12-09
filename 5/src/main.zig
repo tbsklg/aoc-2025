3c92cd7c752600f94fb66724fc120ba7dcb4d5d4
Tobias Klug <tbs.klg@gmail.com>
2025-12-09 13:12:29 +0100

    Solve part 1

--- 5/src/main.zig
+++ 5/src/main.zig
@@ -17,6 +17,9 @@ pub fn main() !void {
 
     const sol_1 = try part_1(trimmed);
     std.debug.print("Solution part 1: {d}\n", .{sol_1});
+    
+    const sol_2 = try part_2(trimmed);
+    std.debug.print("Solution part 2: {d}\n", .{sol_2});
 }
 
 fn part_1(input: []const u8) !usize {
@@ -46,24 +49,38 @@ fn part_1(input: []const u8) !usize {
     return count;
 }
 
+fn part_2(input: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var parts = std.mem.splitSequence(u8, input, "\n\n");
+
+    const ranges = try fold_ranges(allocator, parts.next().?);
+    defer allocator.free(ranges);
+
+    var count: usize = 0;
+    for (ranges) |r| {
+        count += r.to - r.from;
+    }
+
+    return count;
+}
+
 fn fold_ranges(allocator: std.mem.Allocator, input: []const u8) ![]Range {
     var ranges = std.ArrayList(Range){};
     defer ranges.deinit(allocator);
 
     var lines = std.mem.splitScalar(u8, input, '\n');
-    var count: usize = 0;
     while (lines.next()) |line| {
-        count += 1;
         var new_range = try Range.create(line);
 
-        var merged = false;
         for (ranges.items) |*range| {
-            _ = new_range.try_merge(range.*) catch continue;
-            merged = true;
-            break;
-        }
-
-        if (!merged) {
+            if (new_range.try_merge(range.*)) |merged_range| {
+                range.* = merged_range;
+                break;
+            }
+        } else {
             try ranges.append(allocator, new_range);
         }
     }
@@ -86,9 +103,9 @@ const Range = struct {
         };
     }
 
-    fn try_merge(self: *Range, range: Range) !Range {
+    fn try_merge(self: *Range, range: Range) ?Range {
         if (range.to < self.from or range.from > self.to) {
-            return error.NoOverlap;
+            return null;
         }
 
         self.from = @min(self.from, range.from);
@@ -105,25 +122,34 @@ test "Ranges overlap at the bottom" {
     var r1 = try Range.create("10-20");
     const r2 = try Range.create("5-15");
 
-    _ = try r1.try_merge(r2);
+    _ = r1.try_merge(r2);
 
-    try std.testing.expectEqual(r1.from, 5);
-    try std.testing.expectEqual(r1.to, 20);
+    try std.testing.expectEqual(5, r1.from);
+    try std.testing.expectEqual(20, r1.to);
 }
 
 test "Ranges overlap at the top" {
     var r1 = try Range.create("10-20");
     const r2 = try Range.create("15-25");
 
-    _ = try r1.try_merge(r2);
+    _ = r1.try_merge(r2);
 
-    try std.testing.expectEqual(r1.from, 10);
-    try std.testing.expectEqual(r1.to, 25);
+    try std.testing.expectEqual(10, r1.from);
+    try std.testing.expectEqual(25, r1.to);
 }
 
 test "Ranges do not overlap" {
     var r1 = try Range.create("10-20");
     const r2 = try Range.create("21-25");
 
-    try std.testing.expectError(error.NoOverlap, r1.try_merge(r2));
+    try std.testing.expectEqual(null, r1.try_merge(r2));
+}
+
+test "Range has ingredient" {
+    const r1 = try Range.create("10-20");
+
+    try std.testing.expectEqual(true, r1.includes(11));
+    try std.testing.expectEqual(true, r1.includes(20));
+    try std.testing.expectEqual(false, r1.includes(21));
+    try std.testing.expectEqual(false, r1.includes(9));
 }
