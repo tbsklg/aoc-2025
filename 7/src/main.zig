406a7a30883ad4c54e1a50349feea121f3ca4ee3
Tobias Klug <tbs.klg@gmail.com>
2025-12-11 21:34:42 +0100

    Solve part 1

--- 7/src/main.zig
+++ 7/src/main.zig
@@ -11,14 +11,42 @@ pub fn main() !void {
         1024 * 1024,
     );
     defer allocator.free(file_contents);
-    
-    try part_1(allocator, file_contents);
+
+    const trimmed = std.mem.trim(u8, file_contents, "\t\r\n");
+
+    const sol_1 = try part_1(allocator, trimmed);
+    std.debug.print("Solution part 1: {d}\n", .{sol_1});
 }
 
-fn part_1(_: std.mem.Allocator, input: []u8) !void {
+fn part_1(allocator: std.mem.Allocator, input: []const u8) !usize {
     var lines_iter = std.mem.splitScalar(u8, input, '\n');
+    const start = std.mem.indexOf(u8, lines_iter.first(), "S").?;
 
-    while(lines_iter.next()) |line| {
-        std.debug.print("{s}\n", .{line});
+    var current = std.AutoHashMap(usize, void).init(allocator);
+    defer current.deinit();
+    var next = std.AutoHashMap(usize, void).init(allocator);
+    defer next.deinit();
+
+    try current.put(start, {});
+    var split_count: usize = 0;
+
+    while (lines_iter.next()) |line| {
+        var iterator = current.iterator();
+        while (iterator.next()) |x| {
+            const index = x.key_ptr.*;
+            const elem = line[index];
+            if (elem == '^') {
+                split_count += 1;
+                try next.put(index - 1, {});
+                try next.put(index + 1, {});
+            } else {
+                try next.put(index, {});
+            }
+        }
+
+        current.clearRetainingCapacity();
+        std.mem.swap(std.AutoHashMap(usize, void), &current, &next);
     }
+
+    return split_count;
 }
