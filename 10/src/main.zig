f5c67a3c0049575fae34de643d77edda9d416ec5
Tobias Klug <tbs.klg@gmail.com>
2025-12-22 08:51:35 +0100

    Parse buttons

--- 10/src/main.zig
+++ 10/src/main.zig
@@ -24,6 +24,11 @@ fn part_1(allocator: std.mem.Allocator, input: []const u8) !usize {
     while (lines.next()) |line| {
         const m = try Machine.from(allocator, line);
         defer allocator.free(m.lights);
+        defer allocator.free(m.buttons);
+
+        for (m.buttons) |button| {
+            defer allocator.free(button);
+        }
 
         std.debug.print("{any}", .{m});
     }
@@ -33,20 +38,48 @@ fn part_1(allocator: std.mem.Allocator, input: []const u8) !usize {
 
 const Machine = struct {
     lights: []const u8,
+    buttons: [][]const u8,
 
     fn from(allocator: std.mem.Allocator, input: []const u8) !Machine {
         var iter = std.mem.splitScalar(u8, input, ' ');
         const lights = try parse_lights(allocator, iter.next().?);
 
+        var buttons = std.ArrayList([]const u8){};
+
+        while (iter.next()) |part| {
+            if (part[0] == '(') {
+                const button = try parse_button(allocator, part, lights.len);
+                try buttons.append(allocator, button);
+            }
+        }
+
         return .{
             .lights = lights,
+            .buttons = try buttons.toOwnedSlice(allocator),
         };
     }
 };
 
+fn parse_button(allocator: std.mem.Allocator, input: []const u8, len: usize) ![]const u8 {
+    var button = std.ArrayList(u8){};
+
+    for (0..len) |_| {
+        try button.append(allocator, 0);
+    }
+
+    for (input) |c| {
+        if (c == '(' or c == ')' or c == ',') {
+            continue;
+        }
+
+        button.items[c - '0'] = 1;
+    }
+
+    return button.toOwnedSlice(allocator);
+}
+
 fn parse_lights(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
     var lights = std.ArrayList(u8){};
-    defer lights.deinit(allocator);
 
     for (input) |c| {
         if (c == '[' or c == ']') {
