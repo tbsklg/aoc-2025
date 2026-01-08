ccae54564dc465e034b4e905f6899572e85a7041
Tobias Klug <tbs.klg@gmail.com>
2026-01-08 10:22:01 +0100

    12 (#18)

---
+++ 12/src/main.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file_contents = try std.fs.cwd().readFileAlloc(
+        allocator,
+        "input.txt",
+        1024 * 1024,
+    );
+    defer allocator.free(file_contents);
+
+    const trimmed = std.mem.trim(u8, file_contents, "\t\r\n");
+
+    const sol_1 = try part_1(trimmed);
+    std.debug.print("Solution part 1: {d}\n", .{sol_1});
+}
+
+fn part_1(input: []const u8) !usize {
+    var lines = std.mem.splitScalar(u8, input, '\n');
+
+    for (0..30) |_| {
+        _ = lines.next();
+    }
+
+    var count: usize = 0;
+    while (lines.next()) |line| {
+        if ((try Region.from(line)).fits()) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+const Region = struct {
+    width: usize,
+    length: usize,
+    present_counts: [6]usize,
+
+    fn from(input: []const u8) !Region {
+        var parts = std.mem.splitSequence(u8, input, ": ");
+        var lhs = std.mem.splitScalar(u8, parts.next().?, 'x');
+
+        const width = try std.fmt.parseInt(usize, lhs.next().?, 10);
+        const length = try std.fmt.parseInt(usize, lhs.next().?, 10);
+
+        var rhs = std.mem.splitScalar(u8, parts.next().?, ' ');
+
+        var buffer: [6]usize = undefined;
+        var index: usize = 0;
+        while (rhs.next()) |count| {
+            buffer[index] = try std.fmt.parseInt(usize, count, 10);
+            index += 1;
+        }
+
+        return .{
+            .width = width,
+            .length = length,
+            .present_counts = buffer,
+        };
+    }
+
+    fn required_area(self: *const Region) usize {
+        var total: usize = 0;
+        for (self.present_counts) |count| {
+            total += count * 9;
+        }
+        return total;
+    }
+
+    fn area(self: *const Region) usize {
+        return self.width * self.length;
+    }
+
+    fn fits(self: *const Region) bool {
+        return self.area() >= self.required_area();
+    }
+};
